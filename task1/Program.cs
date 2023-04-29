83e5c0ccfdce8dd175edcb58571190ecaf5d7b1c
Murad T. Taghizada <muradtt@code.local>
2023-04-29 12:24:28 +0400

    add

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,3 +1,628 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-
+#region Task1Evveline7Sonuna8
+//L1:
+//Console.Write("4 reqemli ededi daxil edin: ");
+//string strNum = Console.ReadLine();
+//if (!int.TryParse(strNum, out int num))
+//{
+//    goto L1;
+//}
+//int numDigit = (int)Math.Log10(num) +1;
+//int power = (int)Math.Pow(10, numDigit);
+//if (numDigit != 4)
+//{
+//    goto L1;
+//}
+//int firstNum = power * 7 + num;
+//int lastNum = firstNum * 10 + 8;
+//Console.WriteLine($"Netice: {lastNum}");
+#endregion
+#region Task2SonunaOzunuElaveEt
+//L1:
+//Console.Write("3 reqemli ededi daxil edin: ");
+//string strNum = Console.ReadLine();
+//if (!int.TryParse(strNum, out int num))
+//{
+//    goto L1;
+//}
+//int numDigit = (int)Math.Log10(num) + 1;
+//int power = (int)Math.Pow(10, numDigit);
+//if (numDigit != 3)
+//{
+//    goto L1;
+//}
+//int firstNum = power * num + num;
+//Console.WriteLine($"Netice: {firstNum}");
+#endregion
+#region Task3Ededin18FaizSonra3FaizTap
+//L1:
+//Console.Write("5 reqemli ededi daxil edin: ");
+//string strNum = Console.ReadLine();
+//if (!int.TryParse(strNum, out int num))
+//{
+//    goto L1;
+//}
+//int numDigit = (int)Math.Log10(num) + 1;
+//if (numDigit != 5)
+//{
+//    goto L1;
+//}
+//double firstNum = (num * 18)/100;
+//double lastNum = (firstNum * 3)/100;
+//Console.WriteLine($"Verilen ededin 18%-i: {firstNum}%-dir");
+//Console.WriteLine($"Verilen ededin 18%-inin 3%-i: {lastNum}%-dir");
+#endregion
+#region Task4Sonuna7Sonra7Faiztap
+//L1:
+//Console.Write("3 reqemli ededi daxil edin: ");
+//string strNum = Console.ReadLine();
+//if (!int.TryParse(strNum, out int num))
+//{
+//    goto L1;
+//}
+//int numDigit = (int)Math.Log10(num) + 1;
+//if (numDigit != 3)
+//{
+//    goto L1;
+//}
+//double firstNum = num * 10 +7;
+//double lastNum = (firstNum * 7) / 100;
+//Console.WriteLine($"Verilen ededin sonuna 7 elave etdikden sonra alinan ededin 7%-i: {lastNum}%-dir");
+#endregion
+#region Task5Evveline4Sonuna44Faiz44
+//L1:
+//Console.Write("4 reqemli ededi daxil edin: ");
+//string strNum = Console.ReadLine();
+//if (!int.TryParse(strNum, out int num))
+//{
+//    goto L1;
+//}
+//int numDigit = (int)Math.Log10(num) + 1;
+//int power = (int)Math.Pow(10, numDigit);
+//if (numDigit != 4)
+//{
+//    goto L1;
+//}
+//int firstNum = power * 4 + num;
+//int lastNum = firstNum * 100 + 44 ;
+//double percent = (firstNum * 44) / 100;
+//Console.WriteLine($"Verilen ededin evveline 4 ve sonuna 44 elave etdikden sonra alinan ededin 44%-i: {percent}%-dir");
+#endregion
+#region Task6Evvel20FaizSonra10FaizKvadrat
+//L1:
+//Console.Write("4 reqemli ededi daxil edin: ");
+//string strNum = Console.ReadLine();
+//if (!int.TryParse(strNum, out int num))
+//{
+//    goto L1;
+//}
+//int numDigit = (int)Math.Log10(num) + 1;
+//if (numDigit != 4)
+//{
+//    goto L1;
+//}
+//double firstPer = (num * 20) / 100;
+//double lastPer = (firstPer * 10) / 100;
+//double percentPow = (double)Math.Pow(lastPer, 2);
+//Console.WriteLine($"Verilen ededin evvel 20%-inden alinan ededin 10%-inin kvadrati: {percentPow}-dir");
+#endregion
+#region Task7EdedlerinCemiEvvel5Axrina5Faiz5
+//L1:
+//Console.Write("5 reqemli 1-ci ededi daxil edin: ");
+//string strNum1 = Console.ReadLine();
+//if (!int.TryParse(strNum1, out int num1))
+//{
+//    goto L1;
+//}
+//int numDigit1 = (int)Math.Log10(num1) + 1;
+//if (numDigit1 != 5)
+//{
+//    goto L1;
+//}
+//L2:
+//Console.Write("5 reqemli 2-ci ededi daxil edin: ");
+//string strNum2 = Console.ReadLine();
+//if (!int.TryParse(strNum2, out int num2))
+//{
+//    goto L2;
+//}
+//int numDigit2 = (int)Math.Log10(num2) + 1;
+//if (numDigit2 != 5)
+//{
+//    goto L2;
+//}
+//int sum = num1 + num2;
+//int sumDigit = (int)Math.Log10(sum) + 1;
+//int sumPow = (int)Math.Pow(10, sumDigit);
+//int firstSum = 5 * sumPow + sum;
+//int lastSum = (firstSum * 10) + 5;
+//double perSum = (lastSum * 5) / 100;
+//Console.WriteLine($"Verilen ededlerin ceminin evvel ve sonuna 5 artirdiqdan sonra alinan ededin 5%-i: {perSum}-dir");
+#endregion
+#region Task8Ededin4FaiziEdedin9FaiziCemi10Faizi
+//    L1:
+//    Console.Write("4 reqemli 1-ci ededi daxil edin: ");
+//    string strNum1 = Console.ReadLine();
+//    if (!int.TryParse(strNum1, out int num1))
+//    {
+//        goto L1;
+//    }
+//    int numDigit1 = (int)Math.Log10(num1) + 1;
+//    if (numDigit1 != 4)
+//    {
+//        goto L1;
+//    }
+//L2:
+//    Console.Write("7 reqemli 2-ci ededi daxil edin: ");
+//    string strNum2 = Console.ReadLine();
+//    if (!int.TryParse(strNum2, out int num2))
+//    {
+//        goto L2;
+//    }
+//    int numDigit2 = (int)Math.Log10(num2) + 1;
+//    if (numDigit2 != 7)
+//    {
+//        goto L2;
+//    }
+//    double perNum1 = (num1 * 4) / 100;
+//    double perNum2 = (num2 * 9) / 100;
+//    double perSum = ((perNum1 + perNum2) * 10) / 100;
+//    Console.WriteLine($"Verilen 1. ededin 4%-i ile 2. ededin 9%-nin ceminden alinan ededin 10%-i: {perSum}-dir");
+#endregion
+#region Task9HerBirinin10FaizCemi10Faizi
+//L1:
+//    Console.Write("6 reqemli 1-ci ededi daxil edin: ");
+//    string strNum1 = Console.ReadLine();
+//    if (!int.TryParse(strNum1, out int num1))
+//    {
+//        goto L1;
+//    }
+//    int numDigit1 = (int)Math.Log10(num1) + 1;
+//    if (numDigit1 != 6)
+//    {
+//        goto L1;
+//    }
+//L2:
+//    Console.Write("6 reqemli 2-ci ededi daxil edin: ");
+//    string strNum2 = Console.ReadLine();
+//    if (!int.TryParse(strNum2, out int num2))
+//    {
+//        goto L2;
+//    }
+//    int numDigit2 = (int)Math.Log10(num2) + 1;
+//    if (numDigit2 != 6)
+//    {
+//        goto L2;
+//    }
+//L3:
+//    Console.Write("6 reqemli 3-cu ededi daxil edin: ");
+//    string strNum3 = Console.ReadLine();
+//    if (!int.TryParse(strNum3, out int num3))
+//    {
+//        goto L3;
+//    }
+//    int numDigit3 = (int)Math.Log10(num3) + 1;
+//    if (numDigit3 != 6)
+//    {
+//        goto L3;
+//    }
+//    double perNum1 = (num1 * 10) / 100;
+//    double perNum2 = (num2 * 10) / 100;
+//    double perNum3 = (num3 * 10) / 100;
+//    double perSum = ((perNum1 + perNum2 + perNum3) * 10) / 100;
+//    Console.WriteLine($"Verilen 3 ededin her birinin 10%-inin ceminden alinan ededin 10%-i: {perSum}-dir");
+#endregion
+#region Task10Eded1FaizEded2FaizEded3FaizFerqleri3Eded7FaizCemi
+//L1:
+//    Console.Write("4 reqemli 1-ci ededi daxil edin: ");
+//    string strNum1 = Console.ReadLine();
+//    if (!int.TryParse(strNum1, out int num1))
+//    {
+//        goto L1;
+//    }
+//    int numDigit1 = (int)Math.Log10(num1) + 1;
+//    if (numDigit1 != 4)
+//    {
+//        goto L1;
+//    }
+//L2:
+//    Console.Write("4 reqemli 2-ci ededi daxil edin: ");
+//    string strNum2 = Console.ReadLine();
+//    if (!int.TryParse(strNum2, out int num2))
+//    {
+//        goto L2;
+//    }
+//    int numDigit2 = (int)Math.Log10(num2) + 1;
+//    if (numDigit2 != 4)
+//    {
+//        goto L2;
+//    }
+//L3:
+//    Console.Write("4 reqemli 3-cu ededi daxil edin: ");
+//    string strNum3 = Console.ReadLine();
+//    if (!int.TryParse(strNum3, out int num3))
+//    {
+//        goto L3;
+//    }
+//    int numDigit3 = (int)Math.Log10(num3) + 1;
+//    if (numDigit3 != 4)
+//    {
+//        goto L3;
+//    }
+//    double perNum1 = (num1 * 1) / 100;
+//    double perNum2 = (num2 * 2) / 100;
+//    double perNum3 = (num3 * 3) / 100;
+//    double perNum4 = (num3 * 7) / 100;
+//    double perSum = (perNum1 - perNum2 - perNum3) + perNum4;
+//    Console.WriteLine($"Verilen 1-ci ededin 1%-i, 2-cinin 2%-i, 3-cunun 3%-inin ferqlerinin uzerine 3-cunun 7%-i geldikde netice: {perSum}-dir");
+#endregion
+#region Task11Eded1CemEded3Eded2CemEded4HasilEded3Faiz3Ferq
+//L1:
+//    Console.Write("5 reqemli 1-ci ededi daxil edin: ");
+//    string strNum1 = Console.ReadLine();
+//    if (!int.TryParse(strNum1, out int num1))
+//    {
+//        goto L1;
+//    }
+//    int numDigit1 = (int)Math.Log10(num1) + 1;
+//    if (numDigit1 != 5)
+//    {
+//        goto L1;
+//    }
+//L2:
+//    Console.Write("5 reqemli 2-ci ededi daxil edin: ");
+//    string strNum2 = Console.ReadLine();
+//    if (!int.TryParse(strNum2, out int num2))
+//    {
+//        goto L2;
+//    }
+//    int numDigit2 = (int)Math.Log10(num2) + 1;
+//    if (numDigit2 != 5)
+//    {
+//        goto L2;
+//    }
+//L3:
+//    Console.Write("5 reqemli 3-cu ededi daxil edin: ");
+//    string strNum3 = Console.ReadLine();
+//    if (!int.TryParse(strNum3, out int num3))
+//    {
+//        goto L3;
+//    }
+//    int numDigit3 = (int)Math.Log10(num3) + 1;
+//    if (numDigit3 != 5)
+//    {
+//        goto L3;
+//    }
+//L4:
+//    Console.Write("5 reqemli 4-cu ededi daxil edin: ");
+//    string strNum4 = Console.ReadLine();
+//    if (!int.TryParse(strNum4, out int num4))
+//    {
+//        goto L4;
+//    }
+//    int numDigit4 = (int)Math.Log10(num4) + 1;
+//    if (numDigit4 != 5)
+//    {
+//        goto L4;
+//    }
+//    int sum1 = num1 + num3;
+//    int sum2 = num2 + num4;
+//    int total = sum1 * sum2;
+//    int perNum3 = (num3 * 3) / 100;
+//    int result = total - perNum3;
+//    Console.WriteLine($"Verilen 1-ci ededle 3-cunun ve 2-ci ededle 4-cunun cemlerinin hasilinden, 3-cunun 3%-inin ferqinin neticesi: {result}-dir");
+#endregion
+#region Task12Ededlerin10FaizCemile15FaizCemiHasilinin10Faizinin11Faizi
+//L1:
+//    Console.Write("6 reqemli 1-ci ededi daxil edin: ");
+//    string strNum1 = Console.ReadLine();
+//    if (!int.TryParse(strNum1, out int num1))
+//    {
+//        goto L1;
+//    }
+//    int numDigit1 = (int)Math.Log10(num1) + 1;
+//    if (numDigit1 != 6)
+//    {
+//        goto L1;
+//    }
+//L2:
+//    Console.Write("6 reqemli 2-ci ededi daxil edin: ");
+//    string strNum2 = Console.ReadLine();
+//    if (!int.TryParse(strNum2, out int num2))
+//    {
+//        goto L2;
+//    }
+//    int numDigit2 = (int)Math.Log10(num2) + 1;
+//    if (numDigit2 != 6)
+//    {
+//        goto L2;
+//    }
+//L3:
+//    Console.Write("6 reqemli 3-cu ededi daxil edin: ");
+//    string strNum3 = Console.ReadLine();
+//    if (!int.TryParse(strNum3, out int num3))
+//    {
+//        goto L3;
+//    }
+//    int numDigit3 = (int)Math.Log10(num3) + 1;
+//    if (numDigit3 != 6)
+//    {
+//        goto L3;
+//    }
+//L4:
+//    Console.Write("6 reqemli 4-cu ededi daxil edin: ");
+//    string strNum4 = Console.ReadLine();
+//    if (!int.TryParse(strNum4, out int num4))
+//    {
+//        goto L4;
+//    }
+//    int numDigit4 = (int)Math.Log10(num4) + 1;
+//    if (numDigit4 != 6)
+//    {
+//        goto L4;
+//    }
+//    int perNum1 = (num1 * 10) / 100;
+//    int perNum2 = (num2 * 10) / 100;
+//    int perNum3 = (num3 * 10) / 100;
+//    int perNum4 = (num4 * 10) / 100;
+//    int perSum1 = perNum1 + perNum2 + perNum3 + perNum4;
+//    int perNum5 = (num1 * 15) / 100;
+//    int perNum6 = (num2 * 15) / 100;
+//    int perNum7 = (num3 * 15) / 100;
+//    int perNum8 = (num4 * 15) / 100;
+//    int perSum2 = perNum5 + perNum6 + perNum7 + perNum8;
+//    int total = perSum1 * perSum2;
+//    int perTotal1 = (total * 10) / 100;
+//    int perTotal2 = (perTotal1 * 11) / 100;
+//    Console.WriteLine($"Verilen ededlerin 10%-lerinin cemile 15%-leri ceminin hasilinin 10%-nin neticesinin 11%-i: {perTotal2}-dir");
+#endregion
+#region Task13Reqem5Faiz5HasilReqem3Faiz3CemHerBiri10FaizCem
+//L1:
+//    Console.Write("5 reqemli 1-ci ededi daxil edin: ");
+//    string strNum1 = Console.ReadLine();
+//    if (!int.TryParse(strNum1, out int num1))
+//    {
+//        goto L1;
+//    }
+//    int numDigit1 = (int)Math.Log10(num1) + 1;
+//    if (numDigit1 != 5)
+//    {
+//        goto L1;
+//    }
+//L2:
+//    Console.Write("5 reqemli 2-ci ededi daxil edin: ");
+//    string strNum2 = Console.ReadLine();
+//    if (!int.TryParse(strNum2, out int num2))
+//    {
+//        goto L2;
+//    }
+//    int numDigit2 = (int)Math.Log10(num2) + 1;
+//    if (numDigit2 != 5)
+//    {
+//        goto L2;
+//    }
+//L3:
+//    Console.Write("5 reqemli 3-cu ededi daxil edin: ");
+//    string strNum3 = Console.ReadLine();
+//    if (!int.TryParse(strNum3, out int num3))
+//    {
+//        goto L3;
+//    }
+//    int numDigit3 = (int)Math.Log10(num3) + 1;
+//    if (numDigit3 != 5)
+//    {
+//        goto L3;
+//    }
+//L4:
+//    Console.Write("3 reqemli 1-ci ededi daxil edin: ");
+//    string strNum4 = Console.ReadLine();
+//    if (!int.TryParse(strNum4, out int num4))
+//    {
+//        goto L4;
+//    }
+//    int numDigit4 = (int)Math.Log10(num4) + 1;
+//    if (numDigit4 != 3)
+//    {
+//        goto L4;
+//    }
+//L5:
+//    Console.Write("3 reqemli 2-ci ededi daxil edin: ");
+//    string strNum5 = Console.ReadLine();
+//    if (!int.TryParse(strNum5, out int num5))
+//    {
+//        goto L5;
+//    }
+//    int numDigit5 = (int)Math.Log10(num5) + 1;
+//    if (numDigit5 != 3)
+//    {
+//        goto L5;
+//    }
+//    int perNum1 = (num1 * 5) / 100;
+//    int perNum2 = (num2 * 5) / 100;
+//    int perNum3 = (num3 * 5) / 100;
+//    int perSum1 = perNum1 * perNum2 * perNum3;
+//    int perNum5 = (num4 * 3) / 100;
+//    int perNum6 = (num5 * 3) / 100;
+//    int perSum2 = perNum5 + perNum6;
+//    int perTotal1 = (perSum1 * 10) / 100;
+//    int perTotal2 = (perSum2 * 10) / 100;
+//    int sumTotal = perTotal1 + perTotal2;
+//    Console.WriteLine($"Verilen 5 reqemli ededlerin 5%-nin hasili ve 3 reqemli ededlerin 3%-nin ceminin 10%-lerinin cemi: {sumTotal}-dir");
+#endregion
+#region Task14Cem13YapisdirNeticelriCix10Faiz56Cem11Faizi
+//L1:
+//    Console.Write("6 reqemli 1-ci ededi daxil edin: ");
+//    string strNum1 = Console.ReadLine();
+//    if (!int.TryParse(strNum1, out int num1))
+//    {
+//        goto L1;
+//    }
+//    int numDigit1 = (int)Math.Log10(num1) + 1;
+//    if (numDigit1 != 6)
+//    {
+//        goto L1;
+//    }
+//L2:
+//    Console.Write("6 reqemli 2-ci ededi daxil edin: ");
+//    string strNum2 = Console.ReadLine();
+//    if (!int.TryParse(strNum2, out int num2))
+//    {
+//        goto L2;
+//    }
+//    int numDigit2 = (int)Math.Log10(num2) + 1;
+//    if (numDigit2 != 6)
+//    {
+//        goto L2;
+//    }
+//L3:
+//    Console.Write("6 reqemli 3-cu ededi daxil edin: ");
+//    string strNum3 = Console.ReadLine();
+//    if (!int.TryParse(strNum3, out int num3))
+//    {
+//        goto L3;
+//    }
+//    int numDigit3 = (int)Math.Log10(num3) + 1;
+//    if (numDigit3 != 6)
+//    {
+//        goto L3;
+//    }
+//L4:
+//    Console.Write("6 reqemli 4-cu ededi daxil edin: ");
+//    string strNum4 = Console.ReadLine();
+//    if (!int.TryParse(strNum4, out int num4))
+//    {
+//        goto L4;
+//    }
+//    int numDigit4 = (int)Math.Log10(num4) + 1;
+//    if (numDigit4 != 6)
+//    {
+//        goto L4;
+//    }
+//L5:
+//    Console.Write("6 reqemli 5-ci ededi daxil edin: ");
+//    string strNum5 = Console.ReadLine();
+//    if (!int.TryParse(strNum5, out int num5))
+//    {
+//        goto L5;
+//    }
+//    int numDigit5 = (int)Math.Log10(num5) + 1;
+//    if (numDigit5 != 6)
+//    {
+//        goto L5;
+//    }
+//    L6:
+//    Console.Write("6 reqemli 6-ci ededi daxil edin: ");
+//    string strNum6 = Console.ReadLine();
+//    if (!int.TryParse(strNum6, out int num6))
+//    {
+//        goto L6;
+//    }
+//    int numDigit6 = (int)Math.Log10(num6) + 1;
+//    if (numDigit6 != 6)
+//    {
+//        goto L6;
+//    }
+//    long sum = num1 + num2 + num3 + num4 + num5 + num6;
+//    long power = (long)Math.Pow(10, numDigit3);
+//    long paste = power * num1 + num3;
+//    long diff = paste - sum;
+//    long result1 = (diff * 10) / 100;
+//    long result2 = result1 + num5 + num6;
+//    long result3 = (result2 * 11) / 100;
+//    Console.WriteLine($"Verilen ededlerin cemi ve 1. ile 3. ededleri birlesdirilmesinden alinan neticelrin ferqinin uzerine 5. ile 6. ededleri gel neticenin 10%-nin cavabi: {result3}-dir");
+#endregion
+#region Task15UzunShert
+//L1:
+//    Console.Write("3 reqemli 1-ci ededi daxil edin: ");
+//    string strNum1 = Console.ReadLine();
+//    if (!int.TryParse(strNum1, out int num1))
+//    {
+//        goto L1;
+//    }
+//    int numDigit1 = (int)Math.Log10(num1) + 1;
+//    if (numDigit1 != 3)
+//    {
+//        goto L1;
+//    }
+//L2:
+//    Console.Write("3 reqemli 2-ci ededi daxil edin: ");
+//    string strNum2 = Console.ReadLine();
+//    if (!int.TryParse(strNum2, out int num2))
+//    {
+//        goto L2;
+//    }
+//    int numDigit2 = (int)Math.Log10(num2) + 1;
+//    if (numDigit2 != 3)
+//    {
+//        goto L2;
+//    }
+//L3:
+//    Console.Write("4 reqemli 1-ci ededi daxil edin: ");
+//    string strNum3 = Console.ReadLine();
+//    if (!int.TryParse(strNum3, out int num3))
+//    {
+//        goto L3;
+//    }
+//    int numDigit3 = (int)Math.Log10(num3) + 1;
+//    if (numDigit3 != 4)
+//    {
+//        goto L3;
+//    }
+//L4:
+//    Console.Write("4 reqemli 2-ci ededi daxil edin: ");
+//    string strNum4 = Console.ReadLine();
+//    if (!int.TryParse(strNum4, out int num4))
+//    {
+//        goto L4;
+//    }
+//    int numDigit4 = (int)Math.Log10(num4) + 1;
+//    if (numDigit4 != 4)
+//    {
+//        goto L4;
+//    }
+//L5:
+//    Console.Write("5 reqemli 1-ci ededi daxil edin: ");
+//    string strNum5 = Console.ReadLine();
+//    if (!int.TryParse(strNum5, out int num5))
+//    {
+//        goto L5;
+//    }
+//    int numDigit5 = (int)Math.Log10(num5) + 1;
+//    if (numDigit5 != 5)
+//    {
+//        goto L5;
+//    }
+//L6:
+//    Console.Write("5 reqemli 2-ci ededi daxil edin: ");
+//    string strNum6 = Console.ReadLine();
+//    if (!int.TryParse(strNum6, out int num6))
+//    {
+//        goto L6;
+//    }
+//    int numDigit6 = (int)Math.Log10(num6) + 1;
+//    if (numDigit6 != 5)
+//    {
+//        goto L6;
+//    }
+//L7:
+//    Console.Write("6 reqemli ededi daxil edin: ");
+//    string strNum7 = Console.ReadLine();
+//    if (!int.TryParse(strNum7, out int num7))
+//    {
+//        goto L7;
+//    }
+//    int numDigit7 = (int)Math.Log10(num7) + 1;
+//    if (numDigit7 != 6)
+//    {
+//        goto L7;
+//    }
+//    long sum = num1 + num2 + num3 * num4;
+//    long paste1 = sum * 10 + 7 + (num5 + num6);
+//    long paste2 = (num1 * num2) * 10 + 1;
+//    long diffPlus = (paste1 - paste2) + num7;
+//    long diffMinus = diffPlus - (num1 + num2 + num3 + num4);
+//    long percent = (((((diffMinus * 18) / 100) * 3) / 100) * 1) / 100;
+//    long result = percent + (num5 + num6);
+//    Console.WriteLine($"Verilen uzun shertin neticesi: {result}-dir");
+#endregion
